c408dede189956768f4d3498987a6501bd0e22cc
HanJihye <sadalmelik824@gmail.com>
2023-04-22 16:55:37 +0900

    test: 코드 추가

---
+++ FE/src/views/user-mypage/user-mypage.js
@@ -0,0 +1,17 @@
+const addressSearchBtn = document.querySelector(".address__search");
+const userInfoChangeBtn = document.querySelector(".user-info--change");
+const userDeleteBtn = document.querySelector(".user__delete");
+
+const [
+    userEmail,
+    userPassWordOne,
+    userPassWordTwo,
+    userName,
+    userPhoneNumber,
+    userPostCode,
+    userStreetAddress,
+    userExtraAddress,
+] = document.querySelectorAll(".user_info");
+
+// 유저 불러오기
+

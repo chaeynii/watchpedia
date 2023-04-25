3219e533378aa6d39e0e785e929ddbe71dde26ab
ulterWiseWater <agiggompuu@gmail.com>
2023-04-25 17:36:06 +0900

    fix category product Schema

--- BE/src/db/schemas/productSchema.js
+++ BE/src/db/schemas/productSchema.js
@@ -3,22 +3,38 @@ import { Schema } from "mongoose";
 const productSchema = new Schema(
   {
     name: { type: String, unique: true, required: true },
-    category: {
-      // 예를 들어 스마트워치 안에 애플까지 넣는 거를 표현한건데 이렇게 하는게 맞나?
-      nameLarge: {
-        type: String,
+    category: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Category",
       },
-      nameSmall: {
-        type: String,
-      },
+    ],
+    longContent: {
+      type: String,
     },
-    longContent: { type: String },
-    shortContent: { type: String },
-    smallImageURL: { type: String },
-    bigImageURL: { type: String },
-    price: { type: Number, required: true },
-    color: [{ type: String, required: true }], // 이것도 배열로 받아야하나?
-    stock: { type: String, default: 10, required: true },
+    shortContent: {
+      type: String,
+    },
+    smallImageURL: {
+      type: String,
+    },
+    bigImageURL: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    color: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    stock: {
+      type: String,
+      default: 10,
+    },
   },
   { timestamps: true }
 );

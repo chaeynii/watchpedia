2c7439ff0bbe8d4b569d4e2913ba6288756a4cb6
Rocket0000 <gyj960526@gmail.com>
2023-04-27 02:48:00 +0900

    change to login.js file

--- FE/src/views/login/login.js
+++ FE/src/views/login/login.js
@@ -1,3 +1,6 @@
+import * as Api from "/api.js";
+
+
 const loginEmail = document.getElementById("loginInputEmail");
 const loginPassword = document.getElementById("loginInputPw");
 
@@ -58,45 +61,20 @@ async function handleSubmit(e) {
         return;
     }
 
-
-
-// 로그인 api 요청(try,catch 버전) 이거 안넣으면 유효성 검사 alert이 안뜨는 버그...
-//   try {
-//     const data = { email, password };
-
-//     const result = await Api.get("/api/login", data);
-//     const token = result.token;
-
-//     // 로그인 성공, 토큰을 세션 스토리지에 저장
-//     // 물론 다른 스토리지여도 됨
-//     localStorage.setItem("token", token);
-
-//     alert(`정상적으로 로그인되었습니다.`);
-
-//     // 기본 페이지로 이동
-//     window.location.href = "homeTest.html";
-//   } 
-//   catch (err) {
-//     console.error(err.stack);
-//     alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
-//   }
-// }
-
-
 // 로그인 api 요청(fetch 버전)
-const loginURL = "../../../FE/src/views/login/login-dummy.json";
+const loginURL = "/api/login";
 
-const datas = {
-    email: datas.email,
-    pw: datas.pw,
+const data = {
+    email: data.email,
+    pw: data.pw,
 }
 
-fetch(loginURL, {
+Api.post(loginURL, {
     method: "GET",
     headers: {
         "Content-Type": "application/json",
     },
-    body: JSON.stringify(datas) 
+    body: JSON.stringify(data) 
 })
     .then((response) => {
         if (!response.ok) {
@@ -114,4 +92,4 @@ fetch(loginURL, {
         // 에러 처리
         console.error(error);
         // alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`)
-    })
+    })}

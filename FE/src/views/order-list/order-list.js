82f1f8523786b1ce6be402557855635a0e630507
Rocket0000 <gyj960526@gmail.com>
2023-04-29 02:17:57 +0900

    add - 마이페이지 주문 조회

--- FE/src/views/order-list/order-list.js
+++ FE/src/views/order-list/order-list.js
@@ -27,52 +27,72 @@ const addCommas = n => {
 
 
 
+
 // 전체 리스트에서 로그인 한 회원의 주문 내역만 불러오기
 Api.get('/api/admin', "orders")
   .then((data) => {
-     const myOrder = data.filter(i  => i['buyer']._id === userId)
+      const myOrder = data.filter(i  => i['buyer']._id === userId)
+      let orders = myOrder.map((item, i) => item)
 
-     for(let i = 0 ; i < myOrder.length; i++){
-        let orders = myOrder[i]
+      const product = orders.map((item , i)=> item['productInfo'][i])
+      const productCount = orders.map((item, inx) => item.productCount)
+      const orderDate = orders.map(items => items['orderDate'])
+      const shoppingStatus = orders.map(items => items['shoppingStatus'])
+
+      
+      for(let i = 0 ; i < orders.length; i++){
+          
+          orderListZone.innerHTML += `
+        <div class="order__contents card">
+            <div class="card-header">${orderDate[i].slice(0,10)} 주문</div>
+            <div class="orderzone__${i}">
+                <div class="order__${i}" style="
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+                 ">
+                    <div aria-label="card_item">
+                    </div>
+                <div>
+                <div class="detail__zone">
+                    <div class="shipping__status__${i}">${shoppingStatus[i]}</div>
+                        <a type="button" class="btn btn-outline-secondary" href="../order-detail/order-detail.html">
+                            주문상세
+                         </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+    `
+    
+    for(let j = 0; j < product.length; j++){
+        const cardBox = document.querySelectorAll('[aria-label="card_item"]')[i]
+        cardBox.innerHTML += `
+            <a href="" display:"block">
+                <div class="card-body order__body__${j}">
+                    <div class="product__picture">
+                        <img src="${orders[j].productInfo[j].smallImageURL}" class="product__image"/>
+                    </div>
+                    <div class="product__information">
+                        <h5 class="card-title">${orders[j].productInfo[j].name}</h5>
+                        <span class="card-text">₩ ${addCommas(orders[j].productInfo[j].price)}</span>
+                        <span class="card-text">/</span>
+                        <span class="card-text">${productCount[j][j]} 개</span>
+                    </div>
+                </div>     
+            </a>
+        `
+    }
+        
+        // console.log('productssss:::', products[j].name, orders.countList[j], 
+        // products[j].price, products[j].name, products[j].smallImageURL)
+        // console.log('orderssss::', orders[i].orderDate)
         
 
-        console.log('orders', orders)
 
-    //      orderListZone.innerHTML += `<div class="order__contents card">
-    //      <a href="#">
-    //          <div class="card-header">${orders.orderDate} 주문</div>
-    //          <div class="orderzone__${myOrder}" style="display:flex;align-items: center;justify-content: space-between;">
-    //              <div class="order__${myOrder}">
-                     
-    //                  <div class="card-body">
-    //                      <div class="product__picture">
-    //                          <img src="" class="product__image"/>
-    //                      </div>
-    //                      <div class="product__information">
-    //                          <h5 class="card-title">${produtName}</h5>
-    //                          <span class="card-text">${addCommas(produtPrice)}원</span>
-    //                          <span class="card-text"> / </span>
-    //                          <span class="card-text">${orders.countList[i]}개</span>
-    //                      </div>
-    //                  </div>
-                     
-    //              </div>
-    //              <div>
-    //                  <div class="detail__zone">
-    //                      <div class="shipping__status__${myOrder}">${orders.shippingStatus}</div>
-    //                      <a type="button" class="btn btn-outline-secondary" href="../order-detail/order-detail.html">
-    //                          주문상세
-    //                      </a>
-    //                  </div>
-    //              </div>
-    //          </div>
-    //      </a>
-    //  </div>
-    // `;
         
      }
-    
-     console.log('data::::', myOrder)
   })
   .catch((err) => {
     alert(`에러가 발생했습니다. 관리자에게 문의하세요. \n ${err}`);
